661fb7ed487202fc88b08406bcd2896e33f48fdb
Alex-Serky <aserky@gmail.com>
2021-08-28 00:01:44 +0200

    Modifier et supprimer un pokémon

--- app.js
+++ app.js
@@ -27,14 +27,6 @@ app.get('/api/pokemons', (req, res) => {
     res.json(success(message, pokemons))
 })
 
-app.post('/api/pokemons', (req, res) => {
-    const id = getUniqueId(pokemons)
-    const pokemonCreated = { ...req.body, ...{id: id, created: new Date()}}
-    pokemons.push(pokemonCreated)
-    const message = `Le pokémon ${pokemonCreated.name} a bien été créé.`
-    res.json(success(message, pokemonCreated))
-})
-
 // Le nouveau point de terminaison,
 // affichant le nombre total de pokémons :
 app.get('/api/pokemons', (req, res) => {
@@ -48,5 +40,36 @@ app.get('/api/pokemons/:id', (req, res) => {
     const message = "Un pokémon a bien été trouvé."
     res.json(success(message, pokemon))
 })
+
+// L'ajout d'un pokémon
+app.post('/api/pokemons', (req, res) => {
+    const id = getUniqueId(pokemons)
+    const pokemonCreated = { ...req.body, ...{id: id, created: new Date()}}
+    pokemons.push(pokemonCreated)
+    const message = `Le pokémon ${pokemonCreated.name} a bien été créé.`
+    res.json(success(message, pokemonCreated))
+})
+
+// La modification d'un pokémon
+app.put('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const pokemonUpdated = { ...req.body, id: id }
+    pokemons = pokemons.map(pokemon => {
+        return pokemon.id === id ? pokemonUpdated : pokemon
+    })
+
+    const message = `Le pokemon ${pokemonUpdated.name} a bien été modifié.`
+    res.json(success(message, pokemonUpdated))
+})
+
+// La suppression d'un pokemon
+app.delete('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemonDeleted = pokemons.find(pokemon => pokemon.id === id)
+    pokemons = pokemons.filter(pokemon => pokemon.id !== id)
+    const message = `Le pokémon ${pokemonDeleted.name} a bien été supprimé.`
+    res.json(success(message, pokemonDeleted))
+});
+
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
